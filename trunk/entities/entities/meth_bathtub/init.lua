716412b69ca37dceb99cb4d135ef25bcce570a12
pak5695gv <pak5695gv@a5d13965-f633-1203-0095-f173354181b0>
2011-01-02 22:06:32 +0000

    bathtub model update

--- trunk/entities/entities/meth_bathtub/init.lua
+++ trunk/entities/entities/meth_bathtub/init.lua
@@ -7,7 +7,7 @@ include("shared.lua")
 
 function ENT:Initialize()
 
-	self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+	self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 	self.Entity:PhysicsInit(SOLID_VPHYSICS)
 	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
 	self.Entity:SetSolid(SOLID_VPHYSICS)
@@ -46,34 +46,34 @@ function startMixing()
 	if self.Entity:GetNWBool("readyToMix") then
 			local effectdata = EffectData()
 			util.Effect("Sparks", effectdata)
-			self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+			self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			
 			timer.Create("Stage2_"..self:EntIndex(), 1, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage3_"..self:EntIndex(), 2, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage4_"..self:EntIndex(), 3, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage5_"..self:EntIndex(), 4, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage6_"..self:EntIndex(), 5, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage7_"..self:EntIndex(), 6, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 			end)
 			
 			timer.Create("Stage8_"..self:EntIndex(), 10, 1, function()
-				self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+				self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 				self.Entity:SetNWBool("done", true)
 			end)
 			
@@ -97,7 +97,7 @@ function ENT:Use()
 	if self.Entity:GetNWBool("done") then
 		killTimers()
 		self.Entity:SetNWBool("done", false)
-		self.Entity:SetModel("models/props_interiors/BathTub01a.mdl")
+		self.Entity:SetModel("models/props_c17/FurnitureBathtub001a.mdl")
 		local SpawnPos = self.Entity:GetPos()
 		local wetMeth = ents.Create("wet_meth")
 		wetMeth:SetPos(SpawnPos)
